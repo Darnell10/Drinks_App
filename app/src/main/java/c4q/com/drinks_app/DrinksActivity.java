2b0d36e65a355d0f3c2a39a97015da64461cfc52
Darnell O <DarnellOtto@ac.c4q.nyc>
2018-01-20 22:09:07 -0500

    Search Add

--- app/src/main/java/c4q/com/drinks_app/DrinksActivity.java
+++ app/src/main/java/c4q/com/drinks_app/DrinksActivity.java
@@ -1,11 +1,15 @@
 package c4q.com.drinks_app;
 
 import android.os.Handler;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 
@@ -17,10 +21,11 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class DrinksActivity extends AppCompatActivity {
-    Cocktail cocktails;
+public class DrinksActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
+   // Cocktail cocktails;
     private static final String TAG = "HELP!!";
     ArrayList<Cocktail.Drink> drinksList;
+    private DrinkService drinkService;
 
 
     @Override
@@ -29,9 +34,7 @@ public class DrinksActivity extends AppCompatActivity {
         setContentView(R.layout.activity_drinks);
 
 
-
         drinkAPI();
-
 
 
     }
@@ -43,26 +46,31 @@ public class DrinksActivity extends AppCompatActivity {
                 .baseUrl("http://www.thecocktaildb.com/api/json/v1/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
-        DrinkService drinkService = retrofit.create(DrinkService.class);
+        drinkService = retrofit.create(DrinkService.class);
+        String s = "margarita";
+        drinkCall(drinkService, s);
+    }
 
-        final Call<Cocktail> cocktailCall = drinkService.getDrink("1","margarita");
+    private void drinkCall(DrinkService drinkService, String s) {
+        final Call<Cocktail> cocktailCall = drinkService.getDrink("1", s);
         Log.d(TAG, "drinkAPI: " + cocktailCall.request());
 
 
         cocktailCall.enqueue(new Callback<Cocktail>() {
             @Override
             public void onResponse(Call<Cocktail> call, Response<Cocktail> response) {
-                if(response.body().getDrinks() == null){
+                if (response.body().getDrinks() == null) {
                     //TODO show alert dialog telling user there is no drink(s)
                 } else {
                     ArrayList<Cocktail.Drink> drinksList = response.body().getDrinks();
-                    for (Cocktail.Drink s : drinksList){
-                        Log.d(TAG, "onResponse: "+ s.getStrDrink());
+                    for (Cocktail.Drink s : drinksList) {
+                        Log.d(TAG, "onResponse: " + s.getStrDrink());
                     }
                     initRecycler(drinksList);
                 }
 
             }
+
             @Override
             public void onFailure(Call<Cocktail> call, Throwable t) {
                 Log.d(TAG, "Wheres My Drink?" + toString());
@@ -71,7 +79,7 @@ public class DrinksActivity extends AppCompatActivity {
         });
     }
 
-    public void initRecycler(ArrayList<Cocktail.Drink> drinksList){
+    public void initRecycler(ArrayList<Cocktail.Drink> drinksList) {
         RecyclerView drinkRecycler = findViewById(R.id.drinks_recycler);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
 
@@ -80,4 +88,29 @@ public class DrinksActivity extends AppCompatActivity {
         drinkRecycler.setAdapter(cocktailAdapter);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.drinkmenu, menu);
+        MenuItem menuItem = menu.findItem(R.id.drink_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(menuItem);
+        searchView.setOnQueryTextListener(this);
+
+
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        if (newText != null) {
+            drinkCall(drinkService, newText.toLowerCase());
+        } else {
+            drinkCall(drinkService, "margarita");
+        }
+        return true;
+    }
 }
